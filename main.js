08f438b7ab10056d3365da1e1dddb23194636335
Alef Silva <alefs93@gmail.com>
2019-04-04 19:26:07 -0300

    Implementing keyboard listeners and handles

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 /****  GAME CONFIGS */
+
+var gameKeysEnums = {
+    ARROW_UP : "ArrowUp",
+    ARROW_DOWN: "ArrowDown",
+    ARROW_LEFT: "ArrowLeft",
+    ARROW_RIGHT: "ArrowRight"
+}
+
+var snakeDirectionsEnum = {
+    ERROR : 0,
+    RIGHT : 1,
+    LEFT : 2,
+    TOP : 3,
+    BOTTOM  : 4
+};
+
 const maxLineAmount = 21;
 const maxLineSize = 42;
 const totalTiles = maxLineAmount * maxLineSize;
@@ -10,9 +26,9 @@ const startLine = 24;
 const startTile = 439;
 
 /**** --- GAME CONFIGS ------*/
-
-let gameRunning;
-
+let lastSnakeHeadPositionId;
+let isGameRunning = false;
+let currentSnakeDirection = snakeDirectionsEnum.ERROR;
 let snakeHeadId;
 let snakeSize = 5;
 let snakeBodyIdsArray = [];
@@ -30,6 +46,7 @@ function initializeGameMap() {
 
 function initializeSnake() {
     snakeHeadId = startTile;
+    lastSnakeHeadPositionId = snakeHeadId;
     snakeBodyIdsArray.push(snakeHeadId);
     var snakeHeadTileElement = document.getElementById(startTile);
     snakeHeadTileElement.classList.add('snakeBodyTile');
@@ -42,32 +59,47 @@ function initializeSnake() {
 }
 
 function startGame() {
-    gameRunning = true;
+    isGameRunning = true;
+    currentSnakeDirection =  snakeDirectionsEnum.RIGHT;
 
     setTimeout(runningGame, 3000);
 }
 
 function runningGame() {
-    console.log('running game');
-
     moveSnake();
-
     setTimeout(runningGame, 500);
 }
 
 function moveSnake() {
+    let lastPreviousTilePositionId = undefined;
 
     for (let i = 0; i < snakeBodyIdsArray.length; i++) {
         unmarkSnakePosition(snakeBodyIdsArray[i]);
+        const isHeadId = snakeBodyIdsArray[i] === snakeHeadId;
 
-        if (snakeBodyIdsArray[i] === snakeHeadId) {
-            snakeBodyIdsArray[i] = snakeBodyIdsArray[i] + 1;
+        const previousPositionAux = snakeBodyIdsArray[i];
+        
+        if(isHeadId) {
+            switch (currentSnakeDirection) {
+                case snakeDirectionsEnum.TOP:
+                    moveToUpHandle(i);
+                    break;
+                case snakeDirectionsEnum.BOTTOM:
+                    moveToBottomHandle(i);
+                    break;
+                case snakeDirectionsEnum.RIGHT:
+                    moveToRightHandle(i);
+                    break;
+                case snakeDirectionsEnum.LEFT:
+                    moveToLeftHandle(i);
+                    break;
+            }
             snakeHeadId = snakeBodyIdsArray[i];
-
-            markSnakePosition(snakeBodyIdsArray[i]);
-            continue;
+        } else {
+            snakeBodyIdsArray[i] = lastPreviousTilePositionId;
         }
-        snakeBodyIdsArray[i] = snakeBodyIdsArray[i] + 1;
+        
+        lastPreviousTilePositionId = previousPositionAux;
         markSnakePosition(snakeBodyIdsArray[i]);
     };
 }
@@ -80,4 +112,43 @@ function markSnakePosition(snakeBodyTileId) {
 function unmarkSnakePosition(snakeBodyTileId) {
     var snakeTileElement = document.getElementById(snakeBodyTileId);
     snakeTileElement.classList.remove('snakeBodyTile');
+}
+
+function initializeKeyboardHandle() {
+    document.addEventListener('keydown', (event) => {
+        switch (event.key){
+            case gameKeysEnums.ARROW_UP:
+                if(currentSnakeDirection !== snakeDirectionsEnum.BOTTOM)
+                    currentSnakeDirection = snakeDirectionsEnum.TOP;
+                break;
+            case gameKeysEnums.ARROW_DOWN:
+                if(currentSnakeDirection !== snakeDirectionsEnum.TOP)
+                    currentSnakeDirection = snakeDirectionsEnum.BOTTOM;
+                break;
+            case gameKeysEnums.ARROW_LEFT:
+                if(currentSnakeDirection !== snakeDirectionsEnum.RIGHT)
+                    currentSnakeDirection = snakeDirectionsEnum.LEFT;
+                break;
+            case gameKeysEnums.ARROW_RIGHT:
+                if(currentSnakeDirection !== snakeDirectionsEnum.LEFT)
+                    currentSnakeDirection = snakeDirectionsEnum.RIGHT;
+                break;
+        }
+    })
+}
+
+function moveToUpHandle(tileIndex) {
+    snakeBodyIdsArray[tileIndex] = snakeBodyIdsArray[tileIndex] - maxLineSize;
+}
+
+function moveToBottomHandle(tileIndex) {
+    snakeBodyIdsArray[tileIndex] = snakeBodyIdsArray[tileIndex] + maxLineSize;
+}
+
+function moveToRightHandle(tileIndex) {
+    snakeBodyIdsArray[tileIndex] = snakeBodyIdsArray[tileIndex] + 1; 
+}
+
+function moveToLeftHandle(tileIndex) {
+    snakeBodyIdsArray[tileIndex] = snakeBodyIdsArray[tileIndex] - 1; 
 }
