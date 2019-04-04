30d2678f714362a82f3e79d525075875221931c0
Alef Silva <alefs93@gmail.com>
2019-04-03 23:56:39 -0300

    running game and move snake methods

--- main.js
+++ main.js
@@ -20,21 +20,21 @@ let snakeHeadTile = null;
 
 function initializeGameMap() {
     var gameContentDocument = document.getElementsByClassName('gameContent')[0];
-    for(let i = 0; i < totalTiles; i++) {
+    for (let i = 0; i < totalTiles; i++) {
         var newTile = document.createElement('div');
         newTile.id = i;
         newTile.classList.add('gameTile');
         gameContentDocument.appendChild(newTile);
-    } 
+    }
 }
 
 function initializeSnake() {
     snakeHeadId = startTile;
-    snakeBodyIdsArray.push(newSnakeBodyId);
+    snakeBodyIdsArray.push(snakeHeadId);
     var snakeHeadTileElement = document.getElementById(startTile);
     snakeHeadTileElement.classList.add('snakeBodyTile');
 
-    for(i = 0; i < snakeSize; i++) {
+    for (i = 0; i < snakeSize; i++) {
         var newSnakeBodyId = snakeHeadId - snakeBodyIdsArray.length;
         snakeBodyIdsArray.push(newSnakeBodyId);
         document.getElementById(newSnakeBodyId).classList.add('snakeBodyTile');
@@ -44,7 +44,40 @@ function initializeSnake() {
 function startGame() {
     gameRunning = true;
 
-    while(gameRunning) {
-        console.log('se passaram 3 segundos');
-    }
+    setTimeout(runningGame, 3000);
+}
+
+function runningGame() {
+    console.log('running game');
+
+    moveSnake();
+
+    setTimeout(runningGame, 500);
+}
+
+function moveSnake() {
+
+    for (let i = 0; i < snakeBodyIdsArray.length; i++) {
+        unmarkSnakePosition(snakeBodyIdsArray[i]);
+
+        if (snakeBodyIdsArray[i] === snakeHeadId) {
+            snakeBodyIdsArray[i] = snakeBodyIdsArray[i] + 1;
+            snakeHeadId = snakeBodyIdsArray[i];
+
+            markSnakePosition(snakeBodyIdsArray[i]);
+            continue;
+        }
+        snakeBodyIdsArray[i] = snakeBodyIdsArray[i] + 1;
+        markSnakePosition(snakeBodyIdsArray[i]);
+    };
+}
+
+function markSnakePosition(snakeBodyTileId) {
+    var snakeTileElement = document.getElementById(snakeBodyTileId);
+    snakeTileElement.classList.add('snakeBodyTile');
+}
+
+function unmarkSnakePosition(snakeBodyTileId) {
+    var snakeTileElement = document.getElementById(snakeBodyTileId);
+    snakeTileElement.classList.remove('snakeBodyTile');
 }
